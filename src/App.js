f93f7a3575a5359d97a3d766d148d84cdd144b43
nao-noah <naomooo.official@gmail.com>
2021-11-18 21:26:14 -0800

    Add component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Title = styled.div`
+  margin: 100px auto 20px;
+  text-align: center;
+  font-size: 24px;
+`;
+
+const WeatherBox = styled.div`
+  width: 300px;
+  height: 150px;
+  margin: 50px auto 20px;
+  text-align: center;
+  box-shadow: 0 0 38px -12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Weather = styled.div`
+  font-size: 32px;
+`;
+
+const Input = styled.input`
+  display: block;
+  border-radius: 4px;
+  height: 30px;
+  width: 300px;
+  border: 1px solid #ccc;
+  margin: 50px auto;
+`;
 
 function App() {
+  const [weather, setWeather] = useState();
+  const [location, setLocation] = useState("Vancouver");
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(async () => {
+    getApi();
+  }, []);
+
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getApi();
+  };
+
+  const getApi = () => {
+    axios
+      .get("https://api.openweathermap.org/data/2.5/weather", {
+        params: {
+          q: location,
+          appid: process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY,
+        },
+      })
+      .then((res) => {
+        setHasError(false);
+        setWeather(res.data.weather[0].description);
+      })
+      .catch(() => {
+        setHasError(true);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Title>{location}'s Weather</Title>
+      <WeatherBox>
+        <Weather>{hasError ? "Not Found" : weather}</Weather>
+      </WeatherBox>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="location"
+          onChange={handleChange}
+          value={location}
+        />
+      </form>
+    </main>
   );
 }
 
